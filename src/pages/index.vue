c4ddfedee260b1c0c1577510c8b44564f15fd4ea
haoqiyung <101010688+haoqiyung@users.noreply.github.com>
2023-02-11 10:31:33 +0800

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,6 +19,17 @@ const rStore = useRecipeStore()
     <p text="sm" m="b-4">
       好的，今天我们来做菜！
     </p>
-   
+    <p>
+      <ChooseFood />
+      <a
+        m="t-2"
+        border="b-1 dashed"
+        class="inline-flex text-sm text-blue-600 dark:text-blue-400"
+        :href="links.contribute" target="_blank"
+        title="居家菜谱投稿"
+      >
+        立即投稿
+      </a>
+    </p>
   </div>
 </template>
